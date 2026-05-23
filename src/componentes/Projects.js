e27a429b9df8e5a77777666780cc495376b8983f
aaronjimenez18 <aaronjimenez642@gmail.com>
2026-05-23 13:54:50 -0600

    Actualizacion de portafolio y CV

--- src/componentes/Projects.js
+++ src/componentes/Projects.js
@@ -1,6 +1,8 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/cloudy-img.PNG";
+import projImg2 from "../assets/img/djgdl-captura.JPG";
+import projImg3 from "../assets/img/zenith-captura.JPG";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
@@ -13,6 +15,18 @@ export const Projects = () => {
       imgUrl: projImg1,
       projectUrl: "https://cloudymx.netlify.app",
       githubUrl: "https://github.com/aaronjimenez18/cloudy",
+    },
+    {
+      title: "DJ-GDL",
+      description: "Web para venta y cotizacion de servicos de DJ",
+      imgUrl: projImg2,
+      projectUrl: "https://djgdl.netlify.app",
+    },
+    {
+      title: "Zenith-erp",
+      description: "Software as a Service Enterprise Resource Planning",
+      imgUrl: projImg3,
+      projectUrl: "https://zenitherpp.netlify.app",
     },
   ];
 
@@ -30,28 +44,12 @@ export const Projects = () => {
                 >
                   <h2>Proyectos</h2>
                   <p>
-                    Una recopilación de mis trabajos más recientes. Como futuro
+                    Una recopilación de mis trabajos full stack. Como futuro
                     ingeniero, me enfoco en crear soluciones reales, funcionales
                     y escalables.
                   </p>
 
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                    {/* TABS */}
-                    <Nav
-                      variant="pills"
-                      className="project-tabs justify-content-center align-items-center"
-                    >
-                      <Nav.Item>
-                        <Nav.Link eventKey="first">Desarrollo Web</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="second">Diseño UI/UX</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="third">En desarrollo</Nav.Link>
-                      </Nav.Item>
-                    </Nav>
-
                     {/* CONTENEDOR DE CONTENIDO */}
                     <div className="project-content">
                       <Tab.Content
@@ -67,18 +65,6 @@ export const Projects = () => {
                               <ProjectCard key={index} {...project} />
                             ))}
                           </Row>
-                        </Tab.Pane>
-
-                        <Tab.Pane eventKey="second">
-                          <p className="text-center empty-tab-text">
-                            Próximamente diseños en Figma y Adobe XD.
-                          </p>
-                        </Tab.Pane>
-
-                        <Tab.Pane eventKey="third">
-                          <p className="text-center empty-tab-text">
-                            Proyectos de ingeniería y análisis de datos.
-                          </p>
                         </Tab.Pane>
                       </Tab.Content>
                     </div>
